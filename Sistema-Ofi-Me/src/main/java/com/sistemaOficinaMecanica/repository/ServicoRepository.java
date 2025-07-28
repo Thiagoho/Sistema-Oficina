9994c525a10abc76c9560b031b3e7a78e4af967c
Thiago H O S <thiago2006_sales@hotmail.com>
2025-07-28 17:15:47 -0300

    Vamos desenvolver a class serviço Repository.

---
+++ Sistema-Ofi-Me/src/main/java/com/sistemaOficinaMecanica/repository/ServicoRepository.java
@@ -0,0 +1,9 @@
+package com.sistemaOficinaMecanica.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sistemaOficinaMecanica.model.Servico;
+
+public interface ServicoRepository  extends JpaRepository<Servico, Integer>{
+
+}
