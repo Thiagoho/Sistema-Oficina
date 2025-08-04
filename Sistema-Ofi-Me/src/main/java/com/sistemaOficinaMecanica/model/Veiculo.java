2606dd649dedae8cef90cd9d01a95d7cadf280c6
Thiago H O S <thiago2006_sales@hotmail.com>
2025-08-04 11:41:34 -0300

    commit

--- Sistema-Ofi-Me/src/main/java/com/sistemaOficinaMecanica/model/Veiculo.java
+++ Sistema-Ofi-Me/src/main/java/com/sistemaOficinaMecanica/model/Veiculo.java
@@ -2,10 +2,7 @@ package com.sistemaOficinaMecanica.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
